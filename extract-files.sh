fa58b9222d9e993d5fefafa6c9e5b1c4073f0685
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2021-10-15 19:07:19 +0530

    taimen: Add shim for missing symbol in lib-imsvt.so
    * E ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: cannot locate symbol "_ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEb" referenced by "/system_ext/lib64/lib-imsvideocodec.so"
    
    Signed-off-by: Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -59,6 +59,9 @@ function blob_fixup() {
     product/etc/sysconfig/nexus.xml)
         sed -i 's/qualcomm/qualcomm/' "${2}"
         ;;
+    system/lib64/lib-imsvideocodec.so )
+        "${PATCHELF}" --add-needed "lib-imsvtshim.so" "${2}"
+        ;;
     esac
 }
 
